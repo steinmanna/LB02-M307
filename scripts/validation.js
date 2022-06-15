46f7f87983b07916a74f6b37edb4cb2b232a3f80
shara-ahmed <84008349+shara-ahmed@users.noreply.github.com>
2022-06-16 01:11:48 +0200

    validation javascript added and adjusted form structure and styling

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,20 +1,10 @@
-// read form element
-const newsletterform = document.getElementById('newsletterform');
-const name = document.getElementById('name');
-const surname = document.getElementById('surname');
-const email = document.getElementById('email');
-const phone = document.getElementById('phone');
-const she = document.getElementById('she');
-const he = document.getElementById('he');
-const choose = document.getElementById('choose');
-const others = document.getElementById('others');
-const weekly = document.getElementById('weekly');
-const monthly = document.getElementById('monthly');
-const biyearly = document.getElementById('biyearly');
-const agreemail = document.getElementById('agreemail');
-const agreeconditions = document.getElementById('agreeconditions');
-const submit = document.getElementById('submit');
+let VALID;
 
+const form = document.getElementById('form');
+const name = document.getElementById('vorname');
+const surname = document.getElementById('nachname');
+const email = document.getElementById('e-mail');
+const phone = document.getElementById('telefonnummer');
 
 
 // Show input error message
@@ -29,26 +19,29 @@ function showError(input, message) {
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
+
 }
 
 // Check email is valid
 function checkEmail(input) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
         showSuccess(input);
     } else {
-        showError(input, 'Diese E-Mail ist nicht korrekt');
+        showError(input, 'E-Mail ist nicht korrekt');
+        VALID = false;
     }
 }
+
 
 // Check email is valid
 function checkPhone(input) {
     const re = /(\b(0041|0)|\B\+41)(\s?\(0\))?(\s)?[1-9]{2}(\s)?[0-9]{3}(\s)?[0-9]{2}(\s)?[0-9]{2}\b/;
-
     if (re.test(input.value.trim())) {
         showSuccess(input);
     } else {
-        showError(input, 'Diese Nummer ist nicht korrekt');
+        showError(input, 'Telefonnummer ist nicht korrekt');
+        VALID = false;
     }
 }
 
@@ -57,8 +50,9 @@ function checkRequired(inputArr) {
     let isRequired = false;
     inputArr.forEach(function(input) {
         if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`);
+            showError(input, `${getFieldName(input)} wird benötigt`);
             isRequired = true;
+            VALID = false;
         } else {
             showSuccess(input);
         }
@@ -71,11 +65,11 @@ function checkRequired(inputArr) {
 function checkLength(input, min, max) {
     if (input.value.length < min) {
         showError(input,
-            `${getFieldName(input)} must be at least ${min} characters`
+            `${getFieldName(input)} muss mindestens ${min} Buchstaben beinhalten`
         );
     } else if (input.value.length > max) {
         showError(input,
-            `${getFieldName(input)} must be less than ${max} characters`
+            `${getFieldName(input)} es dürfen maximal ${max} Buchstaben beinhalten`
         );
     } else {
         showSuccess(input);
@@ -87,11 +81,11 @@ function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-// Validate form input elements
 function validateForm(){
     if(!checkRequired([name, surname, email, phone])){
-        checkLength(name, 3, 40);
-        checkLength(surname, 3, 40);
+        //Aufgabe: Validierung der Länge für Vorname (2 bis 20) und Nachname (2 bis 50)
+        checkLength(name, 3, 50);
+        checkLength(surname, 3, 50);
         checkEmail(email);
         checkPhone(phone);
     }
@@ -100,8 +94,12 @@ function validateForm(){
 
 // Event listeners
 form.addEventListener('submit', function(e) {
-    //https://www.w3schools.com/jsref/event_preventdefault.asp
+    VALID = true;
     e.preventDefault();
+
     //First validate form
     validateForm();
+    if (VALID){
+        alert('Vielen Dank für Ihre Anmeldung!')
+    }
 });
